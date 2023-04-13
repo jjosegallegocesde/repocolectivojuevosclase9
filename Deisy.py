f1853d69065054cdb8a6bca6fab011f32f27fdd6
Your Name <you@example.com>
2023-04-13 08:45:16 -0500

    nt4 se crea ejemplo

---
+++ Deisy.py
@@ -0,0 +1,8 @@
+class Deisy:
+    
+def __init__(self):
+        self.nombre= "Deisy"
+        self.colorPelo="rubio"
+        self.edad="38"
+    def saludar (self):
+        print ("hola soy deisy...")
