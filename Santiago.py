151af1419a8f967c11603c8594af4fd26baa154d
Nataliacardenas <ncardenasrestrepo@gmail.com>
2023-04-13 08:19:10 -0500

    Clase Santiago creada con 5 atributos y un metodo de saludar

---
+++ Santiago.py
@@ -0,0 +1,11 @@
+class Santiago:
+    
+    def __init__(self):
+        self.nombre = None
+        self.edad = None
+        self.estatura = None
+        self.colorPelo = None
+        self.colorOjos = None
+        
+    def saludar(self):
+        return "Buenas tardes :D"
