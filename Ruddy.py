843b6e464d3799b016351c2860c50e2a489610e6
Ruddy Rodriguez <ruddy.rodriguez.r@hotmail.com>
2023-04-13 08:41:18 -0500

    NT9 se crea clase Ruddy.py

---
+++ Ruddy.py
@@ -0,0 +1,9 @@
+class Ruddy:
+    def __init__(self):
+          self.nombre:"ruddy"
+          self.edad:29
+          self.colorPiel:"Trigueña"
+          self.estatura:1.54
+          self.peso:57
+    def saludar(self):
+         print("Hola chicos, soy Ruddy")
